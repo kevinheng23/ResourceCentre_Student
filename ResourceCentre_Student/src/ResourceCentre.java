c9cd898c41742bb8db6b2e4b76f0cfdda1a667fc
19009231 <19009231@LAPTOP-UMIPNKUR>
2020-07-22 15:45:30 +0800

    line 282 updated (return)

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -275,6 +275,8 @@ public class ResourceCentre {
 	}
 
 	public static boolean doReturnChromebook(ArrayList<Chromebook> chromebookList,String tag){
+		ResourceCentre.viewAllChromebook(chromebookList);
+		String type = Helper.readString("Enter asset tag > ");
 		boolean isReturned = false;
 		// write your code here
 		for (int i = 0; i < chromebookList.size(); i++) {
@@ -292,6 +294,5 @@ public class ResourceCentre {
 	}
 	public static void returnChromebook(ArrayList<Chromebook> chromebookList) {
 		// write your code here
-		
 	}
 }
