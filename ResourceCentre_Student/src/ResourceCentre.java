8ec2231dd2a1282082db1143671d09958ef4ce6e
19009231 <19009231@LAPTOP-UMIPNKUR>
2020-07-22 15:07:12 +0800

    line 88 is updated (return)

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -86,14 +86,38 @@ public class ResourceCentre {
 
 					// Return camcorder
 					ResourceCentre.returnCamcorder(camcorderList);
+					String type = Helper.readString("Enter type > ");
+					boolean isReturned = false;
 
+					for (int i = 0; i < camcorderList.size(); i++) {
+						if (type.equalsIgnoreCase(camcorderList.get(i).getAssetTag())
+								&& camcorderList.get(i).getIsAvailable() == false) {
+							camcorderList.get(i).setIsAvailable(true);
+							camcorderList.get(i).setDueDate("");
+							System.out.println("Camcorder " + type + " returned");
+						}
+					}
+					if (isReturned == false) {
+						System.out.println("Invalid type");
+					}
 				} else if (itemType == 2) {
 
 					// Return Chromebook
 					ResourceCentre.returnChromebook(chromebookList);
+					String type = Helper.readString("Enter type > ");
+					boolean isReturned = false;
 
-				} else {
-					System.out.println("Invalid type");
+					for (int i = 0; i < chromebookList.size(); i++) {
+						if (type.equalsIgnoreCase(chromebookList.get(i).getAssetTag())
+								&& chromebookList.get(i).getIsAvailable() == false) {
+							chromebookList.get(i).setIsAvailable(true);
+							chromebookList.get(i).setDueDate("");
+							System.out.println("Camcorder " + type + " returned");
+						}
+					}
+					if (isReturned == false) {
+						System.out.println("Invalid type");
+		
 				}
 
 			} else if (option == 5) {
@@ -101,7 +125,7 @@ public class ResourceCentre {
 			} else {
 				System.out.println("Invalid option");
 			}
-
+			}
 		}
 
 	}
