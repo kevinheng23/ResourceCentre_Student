e1d60cf2a931963acb5be07296a0408e45a9f436
19022600 <19022600@LAPTOP-TGV9SCVM>
2020-07-22 14:41:52 +0800

    Revert "update doLoanChromebook() in ResourceCentre"
    This reverts commit 238530e04e63a12910524eeba6e4138c1e30e32f.

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -85,46 +85,16 @@ public class ResourceCentreTest {
 	}
 
 	@Test
-	public static boolean doLoanCamcorderTest(ArrayList<Camcorder> camcorderList, String tag, String dueDate) {
+	public void doLoanCamcorderTest() {
 		//fail("Not yet implemented");
 		// write your code here
-		
-		boolean isLoaned = false;
-
-		for (int i = 0; i < camcorderList.size(); i++) {
-			if (tag.equalsIgnoreCase(camcorderList.get(i).getAssetTag())
-					&& camcorderList.get(i).getIsAvailable() == true) {
-				
-				camcorderList.get(i).setIsAvailable(false);
-				camcorderList.get(i).setDueDate(dueDate);
-				
-				isLoaned = true;
-				
-			}
-		}
-		return isLoaned;
-
 		
 	}
 	
 	@Test
-	public static boolean doLoanChromebookTest(ArrayList<Chromebook> chromebookList, String tag, String dueDate) {
+	public void doLoanChromebookTest() {
 		//fail("Not yet implemented");
 		// write your code here
-		boolean isLoaned = false;
-		
-		for (int i = 0; i <  chromebookList.size(); i++) {
-			if (tag.equalsIgnoreCase(chromebookList.get(i).getAssetTag())
-			&& chromebookList.get(i).getIsAvailable() == true ) {
-				
-				chromebookList.get(i).setIsAvailable(false);
-				chromebookList.get(i).setDueDate(dueDate);
-				
-				isLoaned = true;
-				
-			}
-		}
-		return isLoaned;
 	}
 	
 	@Test
